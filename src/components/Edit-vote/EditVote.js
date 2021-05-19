23c709ed646d13d5d5bbb7f310c6816b396c5c84
krishna-s11 <krishnasaxena69@gmail.com>
2021-05-19 21:09:32 +0530

    editvote

--- src/components/Edit-vote/EditVote.js
+++ src/components/Edit-vote/EditVote.js
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react'
 import './editVote.css'
 import firebase from '../../firebase'
 import CardNew from '../Cards-new/CardNew'
+import moment from 'moment'
+import VoteScreen from '../Vote-Screen/VoteScreen'
 
 const db = firebase.firestore()
 const EditVote = () => {
 
     const [competitions,setCompetitions] = useState([])
-
+    const [display,setDisplay] = useState(false);
+    
     // getting all competitions from firestore 
     useEffect(() => {
         db.collection('competitions').get().then(querySnap => {
@@ -15,13 +18,22 @@ const EditVote = () => {
         })
     },[])
 
+    const displayState = () => {
+        setDisplay(false);
+    }
+
     return (
         <div className='edit-vote'>
-            {competitions && competitions.map(competition => {
+         {
+            display?<VoteScreen displayState={displayState} />:null
+         }
+        <ol>
+            {competitions && competitions.map((competition,key) => {
                         return(
-                            <CardNew competition={competition} key={competition.id} />
+                            <li onClick={() => setDisplay(true)}><span>{key+1}</span>{competition.data.title}<span>{moment(competition.data.ends).format("D MMM, YYYY h:mm a")}</span></li>
                         )
                     })}
+        </ol>
         </div>
     )
 }
---
+++ src/components/Vote-Screen/VoteScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './voteScreen.css'
+import {GrClose} from 'react-icons/gr'
+
+const VoteScreen = (props) => {
+    return (
+        <div className='vote-screen'>
+            <div className='vote-box'>
+            <a className='close-vote' onClick={props.displayState}><GrClose /></a>
+                <center><h4>Votes</h4></center>
+                <ol style={{marginTop:'20px'}}>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                    <li><span>1</span>Name<span><input style={{width:'60px'}}></input></span></li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default VoteScreen
