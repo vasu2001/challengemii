efdb978525d7a1cf7a375306dea6059ccb488540
vasu2001 <vasudhruv1920@gmail.com>
2021-06-14 22:15:20 +0530

    winner card + leaderboard fixes

--- src/components/PrizeDetails/PrizeDetails.js
+++ src/components/PrizeDetails/PrizeDetails.js
@@ -5,6 +5,8 @@ import coins from '../../assets/coins.png';
 import { AiOutlineClose } from 'react-icons/ai';
 
 const PrizeDetails = ({ onRedeem, close, data }) => {
+   console.log(data);
+
    return (
       <div className="prize_details">
          <div className="prize_details_container">
